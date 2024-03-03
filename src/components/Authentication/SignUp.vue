8ca28b428fb0e832146fd56e06b1ce74efe89078
Pham Van Tien <tienpv1@toprate.io>
2024-03-04 00:30:28 +0700

    add transition form logn

--- src/components/Authentication/SignUp.vue
+++ src/components/Authentication/SignUp.vue
@@ -2,23 +2,25 @@
     <div class="container d-flex justify-content-center align-items-center">
         <div>
             <form class="form d-flex flex-column rounded-4 px-5">
-            <h1 class="text-center py-5">Đăng nhập</h1>
-            <div class="form-input text-center mb-3">
-                <input class="py-2 px-3 rounded-2" type="text" placeholder="Email">
-            </div>
-            <div class="form-input text-center mb-3">
-                <input class="py-2 px-3 rounded-2" type="password" placeholder="Mật khẩu">
-            </div>
-            <div class="d-flex flex-row-reverse mb-3">
-                <a class="text-decoration-none text-reset" href="#">Quên mật khẩu</a>
-            </div>
-            <div class="text-center px-5 mb-3">
-                <button class="rounded-5 btn btn-dark">Đăng nhập</button>
-            </div>
-            <div class="text-center p-5">
-                <p>Make by I'm Phiến</p>
-            </div>
-        </form>
+                <h1 class="text-center py-5">Đăng nhập</h1>
+                <div class="form-input-auth text-center mb-3 position-relative">
+                    <input class="form-input pt-4 pb-2 px-3 rounded-2" type="text">
+                    <label class="form-input-label">Email</label>
+                </div>
+                <div class="form-input-auth text-center mb-3 position-relative">
+                    <input class="form-input pt-4 pb-2  px-3 rounded-2" type="password">
+                    <label class="form-input-label">Mật khẩu</label>
+                </div>
+                <div class="d-flex flex-row-reverse mb-3">
+                    <a class="text-decoration-none text-reset" href="#">Quên mật khẩu</a>
+                </div>
+                <div class="text-center px-5 mb-3">
+                    <button class="rounded-5 btn btn-dark">Đăng nhập</button>
+                </div>
+                <div class="text-center p-5">
+                    <p>Make by I'm Phiến</p>
+                </div>
+            </form>
         </div>
     </div>
 </template>
@@ -26,6 +28,8 @@
     export default {
         name: 'SignUp'
     }
+
+
 </script>
 <style>
     .container {
@@ -37,7 +41,7 @@
         width: 520px;
     }
 
-    .form-input {
+    .form-input-auth {
         width: 100%;
     }
 
@@ -48,6 +52,21 @@
         color: black;
         line-height: 20px;
     }
+
+    .form-input-label {
+        position: absolute;
+        left: 17px;
+        top: 16px;
+        font-size: 17px;
+        transition: 2s;
+    }
+
+    .form-input:valid ~ .form-input-label
+    .form-input:focus ~ .form-input-label
+
+    /* .form-input:not([value=""]) ~ .form-input-label{
+        font-size: 50px;
+    } */
 
     input:hover {
         border: 1px solid #f4f5f7;
